4d60baed34896bfbd2dd008d614bf82121644a22
Sassamani <123267707+Sassamani@users.noreply.github.com>
2025-02-05 16:47:15 -0500

    Add files via upload

---
+++ src/app/assign-user/assign-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assign-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './assign-user.component.html',
+  styleUrl: './assign-user.component.css'
+})
+export class AssignUserComponent {
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDO_FU6TibzjKkvJhHE5s8pVzux_OcFwTI",
+        authDomain: "user-channel-assignment.firebaseapp.com",
+        projectId: "user-channel-assignment",
+        storageBucket: "user-channel-assignment.firebasestorage.app",
+        messagingSenderId: "534953438818",
+        appId: "1:534953438818:web:07858a35d6c47a9646ab3b"
+    }
+  };
+  
