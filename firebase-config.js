da5637bc0b70190e7fb147f35afa534fd5cd5626
sarah <alexandraamani@icloud.com>
2025-02-16 17:12:05 -0500

    Initial commit

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDbkKKzFHEmYW7Nuf8ZTcaB739uINdMKuo",
+    authDomain: "project-46aec.firebaseapp.com",
+    databaseURL: "https://project-46aec-default-rtdb.firebaseio.com",
+    projectId: "project-46aec",
+    storageBucket: "project-46aec.firebasestorage.app",
+    messagingSenderId: "404234946713",
+    appId: "1:404234946713:web:710f2ba6417637120d5008"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
