5f0bf2d5f1e5e9d8700fbb38c35002e098debe0e
mgreco2004 <marcogreco2004@outlook.com>
2025-03-05 11:09:32 -0500

    fixed jasmine/karma tests

--- src/app/admin-dashboard/admin-dashboard.component.spec.ts
+++ src/app/admin-dashboard/admin-dashboard.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
- 
 import { AdminDashboardComponent } from './admin-dashboard.component';
+import { environment } from '../../environments/environment.development';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+
  
 describe('AdminDashboardComponent', () => {
   let component: AdminDashboardComponent;
@@ -8,7 +12,12 @@ describe('AdminDashboardComponent', () => {
  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AdminDashboardComponent]
+      declarations: [AdminDashboardComponent],
+      providers: [
+        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+        provideFirestore(() => getFirestore()),
+        provideAuth(() => getAuth())
+      ]
     })
     .compileComponents();
  
